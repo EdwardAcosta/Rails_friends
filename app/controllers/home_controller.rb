b1e86f8aca06a7ae736015fa7c22b97465c45c06
Edward Rene Acosta <Eacosta2@satx.rr.com>
2021-12-12 19:03:48 -0600

    added pg to production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   def index
   end
   def about
+    @about_me = "My Name is Edward"
+
+    @answer = "Rails Codemy.com Project"
   end
   def equipment
   end  
